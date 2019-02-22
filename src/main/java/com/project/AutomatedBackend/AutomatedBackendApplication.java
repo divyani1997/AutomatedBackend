492975d66f2ff08d1eabf76f2ed6137d3366d946
divyani1997 <40339472+divyani1997@users.noreply.github.com>
2019-02-22 21:04:00 +0530

    project

---
+++ src/main/java/com/project/AutomatedBackend/AutomatedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.AutomatedBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedBackendApplication.class, args);
+	}
+
+}
